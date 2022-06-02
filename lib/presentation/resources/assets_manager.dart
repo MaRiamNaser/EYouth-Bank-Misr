573210d7da928942148d05b0c779fbea725b707e
AhmedMostafa7474 <36231373+AhmedMostafa7474@users.noreply.github.com>
2022-06-02 18:54:02 +0200

    Add Constrains

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -3,4 +3,6 @@ const String IMAGE_PATH = "assets/images";
 class ImageAssets {
   static const String splashLogo = "$IMAGE_PATH/splash_logo.png";
   static  String smallLogo = "$IMAGE_PATH/splash_logo.png";
+  static String GoalPhoto="assets/images/1654183122090.png";
+  static String monkeyPhoto="assets/images/1654182928956.png";
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,4 +1,6 @@
 class AppStrings{
   static const noRouteFound = "No Route Found";
   static const registerTitle = "Register";
+  static const addGoal="Add Goal";
+  static const addTask="Add Task";
 }
