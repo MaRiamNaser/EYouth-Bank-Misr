3a91f9816cefdd915505793efb1d6eeab0910072
Mariam Nasser <mariamnasser979@gmail.com>
2022-06-13 02:22:26 +0200

    Modified the App strings to be json keys

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,56 +1,42 @@
 class AppStrings{
 
   static const baseUrl = 'http://ec2-54-198-82-67.compute-1.amazonaws.com:5000/';
-  static const noRouteFound = "No Route Found";
-  static const registerTitle = "Register";
-  static const addGoal="Add Goal";
-  static const addTask="Add Task";
-
-  
-  //mariam 
-  static const pleaseEnterYourName = "Please enter your name";
-  static const name = "Full Name";
-  static const continueString = "Continue";
-  static const pleaseEnterYourEmail = "Please enter your email";
-  static const email = "Email";
-  static const pleaseEnterYourPassword = "Please enter your Password";
-  static const password = "Password";
-  static const pleaseEnterYourUserName = "Please enter your user name";
-  static const userName = "User name";
-  static const pleaseEnterYourAge = "Please enter your user Age";
-  static const age = "Age";
-  static const welcome="Welcome";
-  static const setyourGoalsLearnEarn="Set your Goals, Learn & Earn.";
+  static const noRouteFound = "noRouteFound";
+  static const registerTitle = "registerTitle";
+  static const addGoal="addGoal";
+  static const addTask="addTask";
+  static const pleaseEnterYourName = "pleaseEnterYourName";
+  static const name = "name";
+  static const continueString = "continueString";
+  static const pleaseEnterYourEmail = "pleaseEnterYourEmail";
+  static const email = "email";
+  static const pleaseEnterYourPassword = "pleaseEnterYourPassword";
+  static const password = "password";
+  static const pleaseEnterYourUserName = "pleaseEnterYourUserName";
+  static const userName = "userName";
+  static const pleaseEnterYourAge = "pleaseEnterYourAge";
+  static const age = "age";
+  static const welcome="welcome";
+  static const setyourGoalsLearnEarn="setyourGoalsLearnEarn";
   static const Balance="Balance";
   static const EGP ="EGP";
   static const Goals="Goals";
   static const Courses="Courses";
   static const Tasks="Tasks";
   static const Description="Description";
-  static const quiz_no1="Quiz No.1";
+  static const quiz_no1="quiz_no1";
   static const title="title";
-  static const GoalTitlehere="Goal Title here...";
-  static const writeGoalHere="Write Your Goal here";
-  static const TaskTitlehere="Task Title here...";
-  static const writeTaskHere="Write Your Task here";
-  static const add="Add";
-  static const loginTitle = "Login";
+  static const GoalTitlehere="GoalTitlehere";
+  static const writeGoalHere="writeGoalHere";
+  static const TaskTitlehere="TaskTitlehere";
+  static const writeTaskHere="writeTaskHere";
+  static const add="add";
+  static const loginTitle = "loginTitle";
   static const Home = "Home";
   static const Videos = "Videos";
   static const Profile = "Profile";
   static const Setting = "Setting";
   static const Laguage = "Laguage";
-  static const Notification = "Notifivarion";
+  static const Notification = "Notification";
   static const SignOut = "SignOut";
-
-
-
-
-
-
-
-
-
-
-
 }
