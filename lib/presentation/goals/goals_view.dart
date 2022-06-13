0792786090a731eeea072b37831332e3e8ea3f4a
Mariam Nasser <mariamnasser979@gmail.com>
2022-06-13 14:07:43 +0200

    Added Powered by Eyouth tech to splash screen

--- lib/presentation/goals/goals_view.dart
+++ lib/presentation/goals/goals_view.dart
@@ -3,6 +3,8 @@ import 'package:bank_misr/Data/repo/goal_repo.dart';
 import 'package:bank_misr/Data/web_services/goal_services.dart';
 import 'package:bank_misr/presentation/resources/assets_manager.dart';
 import 'package:bank_misr/presentation/resources/font_manager.dart';
+import 'package:bank_misr/presentation/resources/strings_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bank_misr/presentation/resources/styles_manager.dart';
