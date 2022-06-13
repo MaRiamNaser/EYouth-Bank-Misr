d298558164460274d438fcf09b09ba0984092a53
Mariam Nasser <mariamnasser979@gmail.com>
2022-06-13 02:48:50 +0200

    updated main and app with localization using phonix package.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,6 @@
+import 'package:bank_misr/app/app_prefs.dart';
 import 'package:bank_misr/presentation/resources/theme_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 import 'package:flutter/material.dart';
 
@@ -35,9 +37,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+    final AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocal().then((local) => {context.setLocale(local)});
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       onGenerateRoute: RouteGenerator.getRoute,
       initialRoute: Routes.splashRoute,
