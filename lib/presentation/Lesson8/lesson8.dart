e829627bd156420eee10901c8afb5edc1e784734
Omar_Ahmed <omarahmed1998991@gmail.com>
2022-06-29 18:06:13 +0200

    added last version

--- lib/presentation/Lesson8/lesson8.dart
+++ lib/presentation/Lesson8/lesson8.dart
@@ -3,9 +3,12 @@ import 'package:bank_misr/presentation/Lesson8/lesson8Widgets.dart';
 import 'package:bank_misr/presentation/resources/color_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import '../../Data/models/lesson4/lesson4slider.dart';
 import '../../Data/models/lesson8/banknote.dart';
 import '../courses/coursesView.dart';
+import '../lesson2/lesson2.dart';
 import '../resources/assets_manager.dart';
 
 class lesson8 extends StatefulWidget {
@@ -18,15 +21,61 @@ class _lesson8State extends State<lesson8> {
   PageController controller=PageController();
   List<Widget> _list=[];
   int _curr=0;
+  int index=0;
+  final _flutterTts = FlutterTts();
   @override
   void initState() {
     super.initState();
+    speak(EGpound[index].description);
     _list=<Widget>[
       // new Container(child:new Firstpage(moneyimages:EGpound[0].img)),
+      new Container(child:new Pages(image: EGpound[0].img,text: EGpound[0].description,)),
       new Container(child:new Pages(image: EGpound[1].img,text: EGpound[1].description,)),
-      new Container(child:new Pages(image: EGpound[2].img,text: EGpound[2].description,)),
-      new Container(child:new Pages(image: EGpound[3].img,text: EGpound[3].description,)),
+      // new Container(child:new Pages(image: EGpound[3].img,text: EGpound[3].description,)),
        ];
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    stop();
+  }
+  void initializeTts() {
+    _flutterTts.setStartHandler(() {
+      setState(() {
+
+      });
+    });
+    _flutterTts.setCompletionHandler(() {
+      setState(() {
+
+      });
+    });
+    _flutterTts.setErrorHandler((message) {
+      setState(() {
+
+      });
+    });
+    _flutterTts.setPauseHandler(() {
+      setState(() {
+
+      });
+    });
+    _flutterTts.setContinueHandler(() {
+      setState(() {
+
+      });
+    });
+
+    _flutterTts.setSpeechRate(0.4);
+  }
+  void speak(String text) async {
+    await _flutterTts.speak(text);
+  }
+
+  void stop() async {
+    await _flutterTts.stop();
+
   }
   @override
   Widget build(BuildContext context) {
@@ -66,7 +115,8 @@ class _lesson8State extends State<lesson8> {
             FloatingActionButton(
                 backgroundColor: ColorManager.darkPrimary,
                 onPressed: (){
-
+                  index--;
+                  speak(EGpound[index].description);
                   setState(() {
                     controller.jumpToPage(_curr-1);
                   });
@@ -76,7 +126,7 @@ class _lesson8State extends State<lesson8> {
             FloatingActionButton(
                 backgroundColor: ColorManager.darkPrimary,
                 onPressed: (){
-
+                        stop();
                   setState(() {
 
                   });
@@ -87,7 +137,9 @@ class _lesson8State extends State<lesson8> {
               child:Icon(Icons.arrow_forward_outlined ,color: ColorManager.white,),
               backgroundColor:onPressedValue? ColorManager.darkPrimary:Colors.grey,
               onPressed: onPressedValue==true?() {
-                if(_curr==EGpound.length)
+                index++;
+                speak(EGpound[index].description);
+                if(_curr==1)
                  pushNewScreen(context, screen:coursesView(),withNavBar: true,pageTransitionAnimation: PageTransitionAnimation.cupertino);
                 else {
                   setState(() {
--- lib/presentation/lesson5/widget/questions_widget.dart
+++ lib/presentation/lesson5/widget/questions_widget.dart
@@ -1,7 +1,10 @@
+import 'package:bank_misr/presentation/courses/coursesView.dart';
+import 'package:bank_misr/presentation/lesson5/fake_data/categories.dart';
 import 'package:bank_misr/presentation/lesson5/model/category.dart';
 import 'package:bank_misr/presentation/lesson5/model/checkbox_state.dart';
 import 'package:bank_misr/presentation/lesson5/model/option.dart';
 import 'package:bank_misr/presentation/lesson5/model/question.dart';
+import 'package:bank_misr/presentation/lesson5/page/category_page.dart';
 import 'package:bank_misr/presentation/lesson5/page/options_widget.dart';
 import 'package:bank_misr/presentation/resources/color_manager.dart';
 import 'package:bank_misr/presentation/resources/styles_manager.dart';
@@ -22,15 +25,25 @@ class QuestionsWidget extends StatelessWidget {
   }) : super();
 
   @override
-  Widget build(BuildContext context) => PageView.builder(
-        onPageChanged: onChangedPage,
-        controller: controller,
-        itemCount: category?.questions?.length,
-        itemBuilder: (context, index) {
-          final question = category?.questions?[index];
-          return buildQuestion(question: question);
-        },
-    );
+  Widget build(BuildContext context) => WillPopScope(
+    onWillPop: ()async{
+      Navigator.of(context).pop();
+      Navigator.of(context).pop();
+      return false;
+    },
+    child: Scaffold(
+      appBar: AppBar(title: Text("Lesson 5"),),
+      body: PageView.builder(
+            onPageChanged: onChangedPage,
+            controller: controller,
+            itemCount: category?.questions?.length,
+            itemBuilder: (context, index) {
+              final question = category?.questions?[index];
+              return buildQuestion(question: question);
+            },
+        ),
+    ),
+  );
 
   Widget buildQuestion({
   Question? question,
